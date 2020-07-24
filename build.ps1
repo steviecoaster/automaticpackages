19171914f5eec35842ada6d17938e67c8b6f50d1
Stephen Valdinger <stephen@chocolatey.io>
2020-07-24 15:27:44 -0400

    Bit more jigging

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-gci $toolsDir
+$Nuspec = Get-ChildItem $toolsDir -recurse -filter *.nuspec | Select -ExpandProperty FullName
+$Install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyInstall.ps1' | Select -ExpandProperty FullName
+
+$Nuspec
+$Install
