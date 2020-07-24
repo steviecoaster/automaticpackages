875ad0f060467b40200a5c1c174bf4c605926c19
Stephen Valdinger <stephen@chocolatey.io>
2020-07-24 16:41:58 -0400

    Small changes while I get the first version through verifier

--- Universal.ps1
+++ Universal.ps1
@@ -10,13 +10,15 @@ param(
 
 $currentVersion = choco list powershelluniversal --exact -r | ConvertFrom-CSV -Delimiter '|' -Header 'Name', 'Version'
 
+if($null -eq $currentVersion) {
+    $currentVersion.Version = '0.0.0'
+}
 if ([version]$($currentVersion.Version) -lt $DashboardVersion) {
    
     $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
     $Nuspec = Get-ChildItem $toolsDir -recurse -filter *.nuspec | Select -ExpandProperty FullName
     $Install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyInstall.ps1' | Select -ExpandProperty FullName
-
 
     $tempPath = Join-Path -Path $env:TEMP -ChildPath ([GUID]::NewGuid()).GUID
     $null = New-Item $tempPath -ItemType Directory
