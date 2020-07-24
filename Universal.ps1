a866b2e8d8d9e71cd7c36e1b0640667bd4e287fd
Stephen Valdinger <stephen@chocolatey.io>
2020-07-24 16:22:32 -0400

    Version gate it so we don't push over existing packages

--- Universal.ps1
+++ Universal.ps1
@@ -6,9 +6,13 @@ param(
     [Parameter()]
     [String]
     $DashboardDownloadPath = "https://imsreleases.blob.core.windows.net/universal/production/$dashboardVersion/PowerShellUniversal.$dashboardVersion.msi"
-    )
+)
 
-    $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$currentVersion = choco list powershelluniversal --exact -r | ConvertFrom-CSV -Delimiter '|' -Header 'Name', 'Version'
+
+if ([version]$($currentVersion.Version) -lt $DashboardVersion) {
+   
+    $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
     $Nuspec = Get-ChildItem $toolsDir -recurse -filter *.nuspec | Select -ExpandProperty FullName
     $Install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyInstall.ps1' | Select -ExpandProperty FullName
@@ -20,15 +24,16 @@ param(
     $UniversalPath = Join-Path $tempPath $("PowerShellUniversal.$DashboardVersion.msi")
 
     $downloader = [System.net.WebClient]::new()
-    $downloader.DownloadFile($DashboardDownloadPath,$UniversalPath)
+    $downloader.DownloadFile($DashboardDownloadPath, $UniversalPath)
 
     $checksum = (Get-FileHash $UniversalPath).Hash
 
     Remove-Item $tempPath -Recurse -Force
 
-    (Get-Content "$Install").Replace('[[URL]]',"$DashboardDownloadPath") | Set-Content "$Install"
-    (Get-Content "$Install").Replace('[[CHECKSUM]]',"$checksum") | Set-Content "$Install"
-    (Get-Content "$Nuspec").Replace('[[VERSION]]',"$DashboardVersion") | Set-Content "$Nuspec"
+    (Get-Content "$Install").Replace('[[URL]]', "$DashboardDownloadPath") | Set-Content "$Install"
+    (Get-Content "$Install").Replace('[[CHECKSUM]]', "$checksum") | Set-Content "$Install"
+    (Get-Content "$Nuspec").Replace('[[VERSION]]', "$DashboardVersion") | Set-Content "$Nuspec"
 
     choco pack $Nuspec --output-directory="'$($env:Build_ArtifactStagingDirectory)'"
     choco push $((Get-ChildItem $env:Build_ArtifactStagingDirectory -filter *.nupkg).FullName) -s https://push.chocolatey.org --api-key="'$env:ChocolateyKey'"
+}
