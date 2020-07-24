87b62837e9077482bb919e2cf4e78dcdf79a27e8
Stephen Valdinger <stephen@chocolatey.io>
2020-07-24 16:28:55 -0400

    don't need my tester anymore

--- build.ps1
+++
@@ -1,7 +0,0 @@
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$Nuspec = Get-ChildItem $toolsDir -recurse -filter *.nuspec | Select -ExpandProperty FullName
-$Install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyInstall.ps1' | Select -ExpandProperty FullName
-
-$Nuspec
-$Install
