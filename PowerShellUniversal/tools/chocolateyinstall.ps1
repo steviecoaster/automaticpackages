8bcb246364b596111fcd4d0bc5cd255871412ef5
Stephen Valdinger <stephen@chocolatey.io>
2020-07-31 16:17:05 -0400

    Changes to allow for the completely silent installtaion of Universal

--- PowerShellUniversal/tools/chocolateyinstall.ps1
+++ PowerShellUniversal/tools/chocolateyinstall.ps1
@@ -3,6 +3,8 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = '[[URL]]'
 
+$pp = Get-PackageParameters
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
@@ -11,8 +13,13 @@ $packageArgs = @{
   softwareName  = 'PowerShellUniversal*'
   checksum      = '[[CHECKSUM]]'
   checksumType  = 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" SUPPRESSBROWSER=true"
   validExitCodes= @(0, 3010, 1641)
+}
+
+
+if($pp['OpenBrowserOnInstall']){
+  $packageArgs.silentArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 
 Install-ChocolateyPackage @packageArgs
--- Universal.ps1
+++ Universal.ps1
@@ -3,6 +3,7 @@ param(
     [Parameter()]
     [Version]
     $DashboardVersion = $([string](Invoke-WebRequest -UseBasicParsing 'https://imsreleases.blob.core.windows.net/universal/production/version.txt')),
+
     [Parameter()]
     [String]
     $DashboardDownloadPath = "https://imsreleases.blob.core.windows.net/universal/production/$dashboardVersion/PowerShellUniversal.$dashboardVersion.msi"
@@ -17,20 +18,9 @@ if ([version]$($currentVersion.Version) -lt $DashboardVersion) {
    
     $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-    $Nuspec = Get-ChildItem $toolsDir -recurse -filter *.nuspec | Select -ExpandProperty FullName
-    $Install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyInstall.ps1' | Select -ExpandProperty FullName
-
-    $tempPath = Join-Path -Path $env:TEMP -ChildPath ([GUID]::NewGuid()).GUID
-    $null = New-Item $tempPath -ItemType Directory
-
-    $UniversalPath = Join-Path $tempPath $("PowerShellUniversal.$DashboardVersion.msi")
-
-    $downloader = [System.net.WebClient]::new()
-    $downloader.DownloadFile($DashboardDownloadPath, $UniversalPath)
-
-    $checksum = (Get-FileHash $UniversalPath).Hash
-
-    Remove-Item $tempPath -Recurse -Force
+    $Nuspec = Get-ChildItem $toolsDir -recurse -filter *.nuspec | Select-Object -ExpandProperty FullName
+    $Install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyInstall.ps1' | Select-Object -ExpandProperty FullName
+    $checksum = $([string](Invoke-WebRequest -UseBasicParsing "https://imsreleases.blob.core.windows.net/universal/production/$dashboardVersion/PowershellUniversal.$dashboardVersion.msi.sha256"))
 
     (Get-Content "$Install").Replace('[[URL]]', "$DashboardDownloadPath") | Set-Content "$Install"
     (Get-Content "$Install").Replace('[[CHECKSUM]]', "$checksum") | Set-Content "$Install"
