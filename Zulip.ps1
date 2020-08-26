5c8836bb47f1a55cdb18cd95a4dfcabc2d6c620f
Stephen Valdinger <stephen@chocolatey.io>
2020-08-25 21:29:22 -0400

    Add zulip files to start a pipeline

---
+++ Zulip.ps1
@@ -0,0 +1,33 @@
+$latestrelease = (Invoke-RestMethod -Uri "https://api.github.com/repos/zulip/zulip-desktop/releases/latest").assets | 
+    Where-Object { $_.name -match 'msi'}
+
+$binary = $latestrelease.name
+$null = $latestrelease.name -match '(?<version>\d+(\.\d+)+)'
+
+$Version = $matches.version
+
+$currentVersion = choco list zulip --exact -r -s https://chocolatey.org/api/v2| ConvertFrom-CSV -Delimiter '|' -Header 'Name', 'Version'
+
+if($null -eq $currentVersion) {
+    $currentVersion = [pscustomobject]@{'Version' = '0.0.0'}
+}
+if ([version]$($currentVersion.Version) -lt $DashboardVersion) {
+   
+    $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+    $Nuspec = Get-ChildItem $toolsDir -recurse -filter zulip.nuspec | Select-Object -ExpandProperty FullName
+    $Install = Get-ChildItem $toolsDir -Recurse -Filter 'chocolateyInstall.ps1' | Where-Object { $_.FullName -contains 'zulip' } | Select-Object -ExpandProperty FullName 
+    $Install
+
+    $tempPath = Join-Path -Path $env:TEMP -ChildPath ([GUID]::NewGuid()).GUID
+    New-item $tempPath -ItemType Directory
+    Invoke-WebRequest -Uri $latestrelease.browser_download_url -OutFile "$TempDir\$binary"
+    $checksum = ((Get-FileHash $TempDir\$binary).Hash).trim()
+
+    (Get-Content "$Nuspec").Replace('[[VERSION]]', "$Version") | Set-Content "$Nuspec"
+    (Get-Content "$Install").Replace('[[URL]]',"$($latestrelease.browser_download_url)")
+    (Get-Content "$Install").Replace('[[CHECKSUM]]',"$checksum")
+
+    choco pack $Nuspec --output-directory="'$($env:Build_ArtifactStagingDirectory)'"
+    choco push $((Get-ChildItem $env:Build_ArtifactStagingDirectory -filter zulip.*.nupkg).FullName) -s https://push.chocolatey.org --api-key="'$env:ChocolateyKey'"
+}
