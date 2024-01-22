cafd3ab5b6125a57ec0e57008e822613dae759c5
James Ruskin <github@justpowerdown.com>
2024-01-22 14:05:08 +0000

    Fixes the PSU Update Script for >3.x
    The update script was using an API endpoint that seems to be stuck reporting 2.x. As 3.x is now feature complete, and 4.x is in full swing, this is unfortunate!
    
    This changes the update script to source versions from the all-release downloads page, and to use environment variable names as shown in the Update GHA.

--- manual/Universal.ps1
+++ manual/Universal.ps1
@@ -1,19 +1,32 @@
-[cmdletBinding()]
+[CmdletBinding()]
 param(
     [Parameter()]
     [Version]
-    $DashboardVersion = $([string](Invoke-WebRequest -UseBasicParsing 'https://imsreleases.blob.core.windows.net/universal/production/version.txt')),
+    $DashboardVersion = $(
+        (Invoke-WebRequest https://ironmansoftware.com/release/powershell-universal -UseBasicParsing).Links.href.Where{
+            # Get any download URL with an MSI, as we're not interested in theoretical releases without it.
+            # Also, ignore the EventHubClient installers, as it's not a part of this package.
+            $_.EndsWith('.msi') -and $_ -notmatch "\.EventHubClient\."
+        }.ForEach{
+            # Get the version "folder" from the URL, as in $DashboardDownloadPath below.
+            $_.Split('/')[-2]
+        } | Sort-Object {
+            # Prerelease versions will fall to the bottom, as [version] does not support them.
+            # This is okay, because the package currently only publishes production versions.
+            $_ -as [version]
+        } -Descending
+    )[0],
 
     [Parameter()]
     [String]
-    $DashboardDownloadPath = "https://imsreleases.blob.core.windows.net/universal/production/$dashboardVersion/PowerShellUniversal.$dashboardVersion.msi",
+    $DashboardDownloadPath = "https://imsreleases.blob.core.windows.net/universal/production/$DashboardVersion/PowerShellUniversal.$DashboardVersion.msi",
 
     [Parameter()]
     [string]
-    $Checksum = $([string](Invoke-WebRequest -UseBasicParsing "https://imsreleases.blob.core.windows.net/universal/production/$dashboardVersion/PowerShellUniversal.$dashboardVersion.msi.sha256"))
+    $Checksum = $([string](Invoke-WebRequest -UseBasicParsing "https://imsreleases.blob.core.windows.net/universal/production/$DashboardVersion/PowerShellUniversal.$DashboardVersion.msi.sha256"))
 )
 
-$currentVersion = choco list powershelluniversal --exact -r | ConvertFrom-CSV -Delimiter '|' -Header 'Name', 'Version'
+$currentVersion = choco search powershelluniversal --source https://community.chocolatey.org/api/v2/ --exact -r | ConvertFrom-CSV -Delimiter '|' -Header 'Name', 'Version'
 
 if($null -eq $currentVersion) {
     $currentVersion = [pscustomobject]@{'Version' = '0.0.0'}
@@ -30,5 +43,5 @@ if ([version]$($currentVersion.Version) -lt $DashboardVersion) {
     (Get-Content "$Nuspec").Replace('[[VERSION]]', "$DashboardVersion") | Set-Content "$Nuspec"
 
     choco pack $Nuspec --output-directory="'$toolsDir'"
-    choco push $((Get-ChildItem $toolsDir -filter powrshelluniversal.*.nupkg).FullName) -s https://push.chocolatey.org --api-key="'$env:CHOCO_API_KEY'"
+    choco push $((Get-ChildItem $toolsDir -Filter powershelluniversal.*.nupkg).FullName) -s $env:UpstreamUrl --api-key="'$env:ChocoPushSecret'"
 }
