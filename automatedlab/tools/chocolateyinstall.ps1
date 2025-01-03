4d40d84c9ad86bf3844ec3131de8a1dc2ccf462c
Stephen Valdinger <stephen@chocolatey.io>
2025-01-03 09:57:02 -0500

    Add automatedlab package and configure new github action to keep it updated on a schedule

---
+++ automatedlab/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+$url64 = '' 
+ 
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'exe'
+    url64bit       = $url64
+    checksum64     = '' 
+    checksumType64 = 'sha256'   
+    silentArgs     = "/qn /norestart /l*v '$($env:TEMP)\$($env:ChocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log'"
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatedlab/update.ps1
@@ -0,0 +1,23 @@
+Import-Module Chocolatey-AU
+
+function global:au_GetLatest {
+    $LatestRelease = Invoke-RestMethod -UseBasicParsing -Uri "https://api.github.com/repos/automatedlab/automatedlab/releases/latest"
+
+    @{
+        URL64   = ($LatestRelease.assets | Where-Object { $_.name -eq "automatedlab.msi" }).browser_download_url
+        Version = $LatestRelease.tag_name
+    }
+}
+ 
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*(\$)url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            #"(?i)(^\s*checksum64\s*=\s*)('.*')"       = { "$1'$($Latest.Checksum64)'" }
+        }
+
+    }
+}
+
+update -ChecksumFor 64
