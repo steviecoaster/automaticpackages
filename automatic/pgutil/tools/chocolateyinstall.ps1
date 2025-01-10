f1474ee06226cfa64fb633d0ef27d6ae13365df2
Stephen Valdinger <stephen@chocolatey.io>
2025-01-10 08:29:12 -0500

    Add pgutil package
    This commit adds pgutil from Inedo as an
    automatically updating package.

---
+++ automatic/pgutil/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$url = ''
+
+$zipArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url = $url
+  UnzipLocation = $toolsDir
+  Checksum = ''
+  ChecksumType = 'SHA256'
+}
+
+Install-ChocolateyZipPackage @zipArgs
---
+++ automatic/pgutil/update.ps1
@@ -0,0 +1,23 @@
+Import-Module Chocolatey-AU
+
+function global:au_GetLatest {
+    $LatestRelease = Invoke-RestMethod -UseBasicParsing -Uri "https://api.github.com/repos/inedo/pgutil/releases/latest"
+
+    @{
+        URL   = ($LatestRelease.assets | Where-Object { $_.name -eq 'pgutil-win-x64.zip' }).browser_download_url
+        Version = $LatestRelease.tag_name.TrimStart('v')
+    }
+}
+ 
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*(\$)url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum)'"
+            #"(?i)(^\s*checksum64\s*=\s*)('.*')"       = { "$1'$($Latest.Checksum64)'" }
+        }
+
+    }
+}
+
+update -ChecksumFor 32
