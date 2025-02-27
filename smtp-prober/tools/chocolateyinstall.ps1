aa737ae36367203b969a298294890705d37351c7
Stephen Valdinger <stephen@chocolatey.io>
2025-02-27 10:26:23 -0500

    add smtp-prober package.

---
+++ smtp-prober/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.alexnolan.net/software/SMTPProber.exe' # download url, HTTPS preferred
+$filename = Split-Path $url -Leaf
+$fileFullPath = Join-Path $toolsDir -ChildPath $filename
+
+$webfileArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  FileFullPath = $fileFullPath
+  Url = $url
+  checksum = '2AC96F9159E965BBE3A88784168577D65E4605234B13A085DDB612AD88DC86B8'
+  checksumType = 'SHA256'
+}
+
+Get-ChocolateyWebFile @webfileArgs
