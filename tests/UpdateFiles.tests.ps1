838cee45ec98a8f7f69ea4132a94dabfb969accb
Gilbert Sanchez <me@gilbertsanchez.com>
2025-02-28 19:27:08 -0800

    Add Update File checks

---
+++ tests/UpdateFiles.tests.ps1
@@ -0,0 +1,41 @@
+BeforeDiscovery {
+  $script:packages = Get-ChildItem -Path $PSScriptRoot\..\automatic -Directory
+}
+Describe 'Update File <_.Name>' -ForEach $script:packages {
+  BeforeAll {
+    $script:UpdateFile = Join-Path -Path $_.FullName -ChildPath 'Update.ps1'
+    $Tokens = $null
+    $Errors = $null
+    $script:ast = [System.Management.Automation.Language.Parser]::ParseFile(
+      $script:UpdateFile,
+      [ref]$Tokens,
+      [ref]$Errors
+    )
+  }
+
+  It 'Update file exists' {
+    $script:UpdateFile | Should -Exist
+  }
+
+  Context 'File encoding' {
+    It "No update file uses Unicode/UTF-16 encoding" {
+      $script:UpdateFile | Test-FileUnicode | Should -Be $false
+    }
+  }
+  
+  It "Import Chocolatey-AU module" {
+    Get-Content $script:UpdateFile | Select-String -Pattern 'Import-Module Chocolatey-AU'
+  }
+
+  It 'has function au_GetLatest' {
+    $functions = $script:ast.FindAll(
+      {
+        param($Ast)
+        $Ast -is [System.Management.Automation.Language.FunctionDefinitionAst] -and
+        $Ast.Name -eq 'global:au_GetLatest'
+      },
+      $false
+    )
+    $functions | Should -HaveCount 1
+  }
+}
