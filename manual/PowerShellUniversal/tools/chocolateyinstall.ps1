3ff1b38c2e0d6a76c4098aa7edfbec206d0fcf2b
James Ruskin <james@chocolatey.io>
2025-03-19 15:37:54 +0000

    Adds Database Upgrade to PSU Install / BeforeModify
    If the connectionstring doesn't contain upgrade=true, we add it (if the user doesn't specify otherwise).

--- manual/PowerShellUniversal/tools/chocolateyinstall.ps1
+++ manual/PowerShellUniversal/tools/chocolateyinstall.ps1
@@ -17,9 +17,22 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-
 if($pp['OpenBrowserOnInstall']){
   $packageArgs.silentArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+}
+
+# To ensure upgrades go smoothly, we ensure the database will be upgraded.
+# For further details, see: https://forums.ironmansoftware.com/t/upgrade-from-1-5-13-to-2-1-0-now-failing-to-start/5183/4
+if (-not $pp.SkipDatabaseUpgrade) {
+  $AppSettingsPath = (Get-Item "${env:ProgramFiles}*\Universal\appsettings.json").FullName
+  if ($AppSettingsPath) {
+    $AppSettings = Get-Content -Path $AppSettingsPath | ConvertFrom-Json
+
+    if ($AppSettings.Data.ConnectionString -notmatch "(^|;)upgrade=true(;|$)") {
+      $AppSettings.Data.ConnectionString = $AppSettings.Data.ConnectionString.TrimEnd(';') + ";upgrade=true"
+      $AppSettings | ConvertTo-Json | Set-Content $AppSettingsPath
+    }
+  }
 }
 
 Install-ChocolateyPackage @packageArgs
