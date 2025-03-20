1ffc1a33775bde45e1f44e3789b2c7ff9447ca3f
Stephen Valdinger <stephen@chocolatey.io>
2025-03-20 12:17:16 -0400

    (nexushell) put update.ps1 in the repo. Derp.

---
+++ automatic/nexushell/update.ps1
@@ -0,0 +1,28 @@
+Import-Module Chocolatey-AU
+function Copy-Module {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [String]
+        $Version
+    )
+
+    $toolsDir = Join-Path $PSScriptRoot -ChildPath 'tools'
+    $tempDir = New-Item (Join-Path $env:TEMP -ChildPath (New-Guid).Guid) -ItemType Directory
+
+    Save-Module -Name NexuShell -RequiredVersion $Version -Path $tempDir
+    Compress-Archive "$tempDir\Nexus*" -DestinationPath "$toolsDir\NexuShell.zip"
+
+
+
+}
+function global:au_GetLatest {
+    $LatestRelease = Invoke-RestMethod -UseBasicParsing -Uri "https://api.github.com/repos/steviecoaster/nexushell/releases/latest"
+    Copy-Module -Version $LatestRelease.tag_name
+
+    @{
+        Version = $LatestRelease.tag_name.TrimStart('v')
+    }
+}
+
+update -ChecksumFor none
