75e16991e692827c7557a3bfbfc81ba69c1325f6
steviecoaster <steviecoaster@users.noreply.github.com>
2025-06-24 00:27:31 +0000

    Apply Choco package update - automatedlab

--- automatic/automatedlab/tools/chocolateyinstall.ps1
+++ automatic/automatedlab/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$url64 = 'https://github.com/AutomatedLab/AutomatedLab/releases/download/5.57.0/AutomatedLab.msi'
+$url64 = 'https://github.com/AutomatedLab/AutomatedLab/releases/download/5.58.0/AutomatedLab.msi'
 $fileName = SPlit-Path -leaf $url64
 $file = Join-Path $toolsDir -ChildPath $fileName
  
@@ -9,7 +9,7 @@ $downloadArgs = @{
     packageName = $env:ChocolateyPackageName
     FileFullPath = $file
     url64bit = $url64
-    checksum64 = 'a6befa62a6dd5deed76f443a4095c57e7bfe318ad202e29670098e836a3dcbd1'
+    checksum64 = 'c932448a7a75b482eb9d0520425a22deadc90a1b496c01fe934ca133b0e6d40f'
     checksumType64 = 'SHA256'
 }
 
