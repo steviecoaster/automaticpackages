078bd861ca1ea344ce9cf91ea05f4edf13c01588
steviecoaster <steviecoaster@users.noreply.github.com>
2026-01-04 00:34:24 +0000

    Apply Choco package update - pgutil

--- automatic/pgutil/tools/chocolateyinstall.ps1
+++ automatic/pgutil/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-$url = 'https://github.com/Inedo/pgutil/releases/download/v2.2.3.1/pgutil-win-x64.zip'
+$url = 'https://github.com/Inedo/pgutil/releases/download/v2.2.4.4/pgutil-win-x64.zip'
 
 $zipArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url = $url
   UnzipLocation = $toolsDir
-  checksum = '6b32b5cf739c9b70facc5406dc95af5cc8665b6a8a93f5c04615c9a2580e069b'
+  checksum = '3769f707d7d36aee44dab5d48a5fad5f1bdbc7a9eaaa88cc90e4a66f57f4aa62'
   checksumType = 'SHA256'
 }
 
