b1a0d0603992c6f6b32da62a3f6a7dc86bdf1817
steviecoaster <steviecoaster@users.noreply.github.com>
2026-06-30 01:04:03 +0000

    Apply Choco package update - inedohub

--- automatic/inedohub/tools/chocolateyinstall.ps1
+++ automatic/inedohub/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = "Stop"
 $toolsdir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$url = 'https://proget.inedo.com/upack/Products/download/InedoReleases/DesktopHub/1.6.0?contentOnly=zip'
+$url = 'https://proget.inedo.com/upack/Products/download/InedoReleases/DesktopHub/1.6.1?contentOnly=zip'
 $pp = Get-PackageParameters
 
 # Create the unzip location
@@ -15,7 +15,7 @@ $zipArgs = @{
   PackageName   = $env:ChocolateyPackageName
   Url           = $url
   UnzipLocation = $unzipLocation
-  Checksum      = 'E891450EC0C4496457E156F12F2B5AE67AA6B65062F95F272627E364B1164E37'
+  Checksum      = '6C25A8526DF53940B32FA6DEAB006760ED7889322ACFA1EB39E6B190D8F98EA1'
   ChecksumType  = 'SHA256'
 }
 
