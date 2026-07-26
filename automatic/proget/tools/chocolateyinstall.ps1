37b7f0066fafac5f09bbe13069733cdb9525e482
steviecoaster <steviecoaster@users.noreply.github.com>
2026-07-26 00:50:04 +0000

    Apply Choco package update - proget

--- automatic/proget/tools/chocolateyinstall.ps1
+++ automatic/proget/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ else {
     throw "Inedo Hub not found, installation cannot continue"
 }
 
-$version = '26.0.6'
+$version = '26.0.7'
 #Set the location of hub.exe for command execution
 $fileLocation = (Get-Command hub.exe).source
 
